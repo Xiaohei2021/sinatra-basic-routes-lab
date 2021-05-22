c4686e99505ba034041d7b786ab318ee805c5180
Leon <hualiang.tian@gmail.com>
2021-05-21 22:36:35 -0400

    commit tree 66f2fd8608d557bb0201fa3ae7482e17f0c735cd

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is Leon"
+end
+
+get '/hometown' do
+    "My hometown is Henan"
+end
+
+get '/favorite-song' do
+    "My favorite song is sold out"
+end
+
+
+
 end
